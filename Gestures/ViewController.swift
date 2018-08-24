ae1e4398d445ba07ca41fe3db7cca09f705d1a29
QuinnPan <quinnpan@tencent.com>
2018-08-24 16:55:08 +0800

    support tap event

--- Gestures/ViewController.swift
+++ Gestures/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imaggView : UIImageView!
+    var img1:UIImage!
+    var img2:UIImage!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let rect = CGRect(x: 0, y: 80, width: 320, height: 320)
+        self.imaggView = UIImageView(frame: rect)
+        img1 = UIImage(named: "img1")
+        self.imaggView.image = img1;
+        img2 = UIImage(named: "img2")
+        
+        imaggView.isUserInteractionEnabled = true
+        view.addSubview(self.imaggView)
+        
+        let guesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.singleTap))
+        imaggView.addGestureRecognizer(guesture)
+        
     }
-
+    @objc func singleTap() {
+        if imaggView.image == self.img1{
+            imaggView.image = self.img2
+        }else{
+            imaggView.image = img1
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
